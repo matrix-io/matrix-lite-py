3150c3291601403ab35bcf52230e99626467d377
Carlos <chacin.carlos9798@gmail.com>
2019-03-05 23:33:37 -0500

    Added initial sensor calls

--- matrix_lite/sensors.py
+++ matrix_lite/sensors.py
@@ -1,16 +1,25 @@
-from halSwig import imu, uv, humidity, pressure
-
-# imu_struct
-
+from halSwig import imu, imu_values
+from halSwig import uv, uv_values
+from halSwig import humidity, humidity_values
+from halSwig import pressure, pressure_values
+      
+# Print statement for each sensor
 def sensorPrint(self):
-    return "Yaw %d" % (self.read().yaw)
+    # return "Yaw %d" % (self.yaw)
+    return "NOT YET IMPLEMENTED. DO THIS CMON WE WAITING!"
 
-imu.__str__ = sensorPrint
-
-
-
+# IMU
+imu_values.__str__ = sensorPrint
 imu = imu()
 
+# UV
 uv = uv()
+uv.__str__ = sensorPrint
+
+# Humidity
 humidity = humidity()
+humidity.__str__ = sensorPrint
+
+# Pressure
 pressure = pressure()
+pressure.__str__ = sensorPrint

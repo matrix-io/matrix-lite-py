b7a5a5b2f9d8978fe61c25d5359ee295d29760c7
Calos Chacin <chacin.carlos9798@gmail.com>
2019-03-22 18:39:22 -0400

    Set LEDs from tuples, strings, & dicts

--- matrix_lite/led.py
+++ matrix_lite/led.py
@@ -1,42 +1,40 @@
 import colour
-from halSwig import everloop, led as ledConfig
+from halSwig import everloop, led
 
-_led = everloop()
-length = _led.ledCount
+_everloop = everloop()
+length = _everloop.ledCount
 
 # Sets the current everloop image on MATRIX Device
 def set(config = None):
-    everloop = []
-
-    # # Default argument turns LEDs off
-    # if config == None:
-    #     _led.set([])
-
-    # # Handle list argument
-    # elif isinstance(config, list):
-    #     print("you inserted a list")
-
-    # # Handle string argument
-    # elif isinstance(config, str):
-    #     print("you inserted a string")
-    # else:
-    #     raise Exception("you inserted an invalid input")
+    # Determine input type #
+    # Set everloop LEDs for each list value
+    if isinstance(config, list):
+        print("You put a list, but this isn't done yet... come back later")
+    # Set everloop as one value
+    elif isinstance(config, str) or isinstance(config, dict) or isinstance(config, tuple):
+        rgbw = _readColor(config)
+        newLed = led(rgbw['r'],rgbw['g'],rgbw['b'],rgbw['w'])
+        _everloop.set([led(rgbw['r'],rgbw['g'],rgbw['b'],rgbw['w'])] * length)
+    # Throw error on invalid input
+    else:
+        raise Exception("led.set() only accepts a value or list of values from: strings, tuples & dicts")
 
 # Returns RGBW of inputs: string, tuple, & dict
 def _readColor(color):
     # Handle dict input
     if isinstance(color, dict):
-        return color
-
+        return {"r": color.get("r", 0), "g": color.get("g", 0),"b": color.get("b", 0),"w": color.get("w", 0),}
+    
+    # Handle string/tuple input #
     rgbw = [0, 0, 0, 0]
+    offset = 1 # offset for colour.Color().rgb decimal return
     
     # Convert string input to tuple
-    offset = 1
     if isinstance(color, str):
         color = colour.Color(color).rgb
-        offset = 255 # offset for .rgb decimal return
+        offset = 255
 
-    # Handle tuple input
+    # Convert tuple to dict
     for i, value in enumerate(color):
         rgbw[i] = int(color[i]*offset)
 

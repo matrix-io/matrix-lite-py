5718769a516d95b65ee5f3f3173228056a1bfe00
Calos Chacin <chacin.carlos9798@gmail.com>
2019-03-25 13:05:19 -0400

    Included halSwig info

--- matrix_lite/__init__.py
+++ matrix_lite/__init__.py
@@ -5,6 +5,8 @@ __all__ = ["led", "gpio", "sensors"]
 from matrix_lite import led, sensors
 
 # Direct HAL SWIG implementations
-# Below are HAL features that don't need abstractions yet
-from halSwig import gpio
+from halSwig import gpio, info
+
+# Below are HAL implementations that don't need abstractions yet
 gpio = gpio()
+info = info()
